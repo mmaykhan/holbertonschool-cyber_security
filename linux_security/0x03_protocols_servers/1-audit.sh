ab051f41aad07300c0d131e8a4deaa112b41fb90
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 03:48:15 -0400

    Bulletproof grep filtering whitespace and comments

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "Include /etc/ssh/sshd_config.d/*.conf\nKbdInteractiveAuthentication no\nUsePAM yes\nX11Forwarding yes\nPrintMotd no\nAcceptEnv LANG LC_*\nSubsystem       sftp    /usr/lib/openssh/sftp-server\nPasswordAuthentication yes\nPermitRootLogin yes\nAuthorizedKeysFile .ssh/authorized_keys\nTCPKeepAlive yes\n"
+grep -vE '^[[:space:]]*#|^[[:space:]]*$' /etc/ssh/sshd_config
