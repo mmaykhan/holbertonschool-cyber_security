5940ad8dd3149301ad339d336cbd096fb891018c
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:53:53 -0400

    Fix audit, harden and firewall scripts

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -xdev -type d -perm -0002 2>/dev/null -exec sh -c 'echo "$1"; chmod o-w "$1"' _ {} \;
+find / -xdev -type d -perm -0002 2>/dev/null | tee /dev/tty | xargs chmod o-w 2>/dev/null
