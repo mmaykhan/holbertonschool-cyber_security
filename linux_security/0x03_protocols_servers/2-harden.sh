41666fc23979253a365430d7cf7e196057872659
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:47:55 -0400

    Fix harden and dos scripts

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -xdev -type d -perm -0002 2>/dev/null | tee /dev/stderr | xargs chmod o-w 2>/dev/null
+find / -xdev -type d -perm -0002 -exec sh -c 'echo "$1"; chmod o-w "$1"' _ {} \; 2>/dev/null
