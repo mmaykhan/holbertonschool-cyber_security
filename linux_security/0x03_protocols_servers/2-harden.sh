91e15d897a1620f8870d8a6a71b1c13554ae37ef
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:18:01 -0400

    Fix harden script

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 2>/dev/null
-find / -type d -perm -0002 -exec chmod o-w {} \; 2>/dev/null
+find / -type d -perm -0002 -print -exec chmod o-w {} \; 2>/dev/null
