796466e1ca559750a0d8249db9bb9713d836b253
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:49:36 -0400

    Add SSH audit, world-writable hardening, and iptables firewall scripts

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -xdev -type d -perm -0002 -exec sh -c 'echo "$1"; chmod o-w "$1"' _ {} \; 2>/dev/null
+find / -xdev -type d -perm -0002 2>/dev/null -exec sh -c 'echo "$1"; chmod o-w "$1"' _ {} \;
