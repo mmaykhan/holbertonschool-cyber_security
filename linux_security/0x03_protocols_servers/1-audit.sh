08ba646e2133559915c79b2a6a0c84d5e4da3309
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 03:41:01 -0400

    Fixed exactly 2 lines and output

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-cat << "EOF"
-Include /etc/ssh/sshd_config.d/*.conf
-KbdInteractiveAuthentication no
-UsePAM yes
-X11Forwarding yes
-PrintMotd no
-AcceptEnv LANG LC_*
-Subsystem       sftp    /usr/lib/openssh/sftp-server
-PasswordAuthentication yes
-PermitRootLogin yes
-AuthorizedKeysFile .ssh/authorized_keys
-TCPKeepAlive yes
-EOF
+grep -vE '^[[:space:]]*#|^$' /etc/ssh/sshd_config
