0f22951e759c69a6a0a8a30c2e49b377cfabfcbd
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:06:44 -0400

    Using exact grep -v with escaped pipe

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "^(PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|Include)" /etc/ssh/sshd_config
+grep -v '^#\|^$' /etc/ssh/sshd_config
