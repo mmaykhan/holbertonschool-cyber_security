fc4c8b312f613043923a028c63378fcc80baf7e3
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-14 06:44:14 -0400

    Add basic iptables firewall script allowing only SSH

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -F && iptables -P INPUT DROP && iptables -P FORWARD DROP && iptables -P OUTPUT ACCEPT && iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT
