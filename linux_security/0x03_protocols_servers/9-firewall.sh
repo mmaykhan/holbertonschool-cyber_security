4d05bf00813e18c16d0412a803f16158db71953e
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-16 06:40:12 -0400

    Add task 9 basic firewall rules blocking all except SSH

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-iptables -A INPUT -p tcp -s "$1" --dport 22 -j ACCEPT
-iptables -P INPUT DROP
+iptables -F && iptables -P INPUT DROP && iptables -P FORWARD DROP && iptables -P OUTPUT ACCEPT && iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT
