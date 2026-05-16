d345f4ed4b4464d95f6a2a7226b03b361c44bc0b
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-05-16 06:55:03 -0400

    Implement exact 3-line firewall rule for SSH

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to set up basic iptables firewall rules that block all incoming traffic except SSH
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT && iptables -P INPUT DROP
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+iptables -P INPUT DROP
