6d5d0d57c47fe5a10b6cc44bc0e1f86994b92e44
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-06-11 13:42:26 -0400

    Final absolute clean fix for task 0 with strict block parsing

--- linux_security/0x01_passive_reconnaissance/0-whois.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-whois $1 | awk -F':[ 	]*' 'BEGIN{split("Name,Organization,Street,City,State/Province,Postal Code,Country,Phone,Phone Ext,Fax,Fax Ext,Email",f,","); split("Registrant,Admin,Tech",p,",")} /^Registrant|^Admin|^Tech/{k=$1; v=$2; sub(/^[ 	]+|[ 	]+$/, "", v); data[k]=v} END{for(i=1;i<=3;i++){for(j=1;j<=12;j++){cn=p[i]" "f[j]; val=data[cn]; fn=f[j]; if(fn~"Ext") fn=fn":"; if(fn~"Street" && val!="") val=val" "; printf "%s,%s
-", p[i]" "fn, val}}}' | grep -v "^," > $1.csv
