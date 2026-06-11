b40d4b829a0641d6330e2773c88b2486f10808a1
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-06-11 13:33:14 -0400

    Fix task 0 with strict awk ordering

--- linux_security/0x01_passive_reconnaissance/0-whois.sh
+++ linux_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-whois $1 | awk -F: '/^(Registrant|Admin|Tech)/ {gsub(/^[ 	]+|[ 	]+$/, "", $2); if ($1 ~ /Street$/) $2=$2" "; print $1 "," $2}' | sed 's/Ext$/Ext:/g' > $1.csv
+whois $1 | awk -F':[ 	]*' 'BEGIN{split("Name,Organization,Street,City,State/Province,Postal Code,Country,Phone,Phone Ext,Fax,Fax Ext,Email",f,","); split("Registrant,Admin,Tech",p,",")} /^Registrant|^Admin|^Tech/{k=$1; v=$2; sub(/^[ 	]+|[ 	]+$/, "", v); data[k]=v} END{for(i=1;i<=3;i++){for(j=1;j<=12;j++){cn=p[i]" "f[j]; val=data[cn]; fn=f[j]; if(fn~"Ext") fn=fn":"; if(fn~"Street" && val!="") val=val" "; printf "%s,%s
+", p[i]" "fn, val}}}' | grep -v "^," > $1.csv
