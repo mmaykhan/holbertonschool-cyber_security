1c846c905f2ebac8337dd5c2f4f03b9fd0b89e61
nocontextblaze <mmmdzadayxan928@gmail.com>
2026-06-11 14:01:16 -0400

    Fix: fresh git tree with corrected awk script

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
+whois "$1" | awk -F: '/^[ \t]*(Registrant|Admin|Tech)/ {k=$1; v=$2; gsub(/^[ \t]+|[ \t]+$/, "", k); gsub(/^[ \t]+|[ \t]+$/, "", v); if (k ~ /Ext/) k=k":"; if (k ~ /Street/) v=v" "; if (k ~ /PostalCode/) k="Postal Code"; if (k ~ /State/) k="State/Province"; print k "," v}' > "${1}.csv"
